b7871d589ef12b85438d6e7adae340d43ee0035c
Rodrigomuina <Rodrigoemanuelmuina991@gmail.com>
2025-04-29 04:19:07 +0200

    Paso6_Contador_de_instancias

--- ejercicio_senales/scenes/Menu/Menu.gd
+++ ejercicio_senales/scenes/Menu/Menu.gd
@@ -9,3 +9,8 @@ func _ready():
 	# Constante "INSTANCES_TEXT" más el valor de "instancesCount" como Texto
 	$CountLabel.text = INSTANCES_TEXT + str(instancesCount)
 	pass
+
+func _count_new_instance():
+	instancesCount += 1
+	$CountLabel.text + str(instancesCount)
+	
