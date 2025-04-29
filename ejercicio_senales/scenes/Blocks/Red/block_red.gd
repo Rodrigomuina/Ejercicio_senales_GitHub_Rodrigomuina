19da45a3b4d27fafb80677a8de8086165f8f0711
Rodrigomuina <Rodrigoemanuelmuina991@gmail.com>
2025-04-29 03:55:48 +0200

    Paso5_Agregando_reset

--- ejercicio_senales/scenes/Blocks/Red/block_red.gd
+++ ejercicio_senales/scenes/Blocks/Red/block_red.gd
@@ -10,6 +10,5 @@ func _ready():
 func _process(delta: float) -> void:
 	pass
 
-
 func delete():
 	queue_free()
--- ejercicio_senales/scenes/Blocks/Violet/block_violet.gd
+++ ejercicio_senales/scenes/Blocks/Violet/block_violet.gd
@@ -9,6 +9,5 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
 func delete():
 	queue_free()
